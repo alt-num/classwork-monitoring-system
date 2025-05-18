b4feb5d633b633b0c05f33628e629bba80633812
Altuminum <ibigfeet0@gmail.com>
2025-05-19 01:02:21 +0800

    Just gonna put the members

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -273,9 +273,42 @@
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
                             <a href="https://github.com/Altuminum" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                </svg>
+                                <span class="relative group">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                    </svg>
+                                    <span class="absolute z-10 left-1/2 -translate-x-1/2 bottom-8 w-64 p-3 rounded-lg bg-gray-900 text-white text-xs shadow-lg opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-pre-line">
+1. Lagramada, Rodel Fernando Balajeboco
+22-02576
+
+2. Kenn Henzly Lomuntad Ciar 
+22-02424
+
+3. Darang, Jasmin C
+22-00290
+
+4. Elizabeth cadilo
+22-02380
+
+5. Lorenz Joshua Campoto
+22-02388
+
+6. Basilia, Merophel N
+22-02361
+
+7. Francis William T. Hermoso
+19-46650
+
+8. Darol, Kyle Bryant A.
+22-02264
+
+9. Negrillo, Arvin O.
+22-02349
+
+10. Nervez, Ryan James M.
+22-02291
+                                    </span>
+                                </span>
                                 Follow
                             </a>
                         </div>
